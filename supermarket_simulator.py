f5dc80ec88444e4dc1318b6a3ac1305f05821821
Shroth <49244343+peachhirai1109@users.noreply.github.com>
2020-07-15 12:22:50 -0400

    Version 0.1
    Created a dictionary with item-price pairs and printed all values in the dictionary.

---
+++ supermarket_simulator.py
@@ -0,0 +1,6 @@
+available_items = {"milk": 1.99, "eggs": 2.39, "bread": 0.99, "water": 1.49, "soda": 2.50, "apples": 0.99, "banana": 0.50, "chips": 1.00}
+
+for item in available_items:
+    print(item)
+
+
